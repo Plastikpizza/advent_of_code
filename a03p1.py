391db548581279e3f8a44472ce29b2919bf44094
Plastikpizza <quastenflosser@ist-einmalig.de>
2017-12-03 21:19:23 +0200

    shorted pr0gr4m

--- a03p1.py
+++ a03p1.py
@@ -12,33 +12,20 @@ def main(search):
     v = 2
     horizontal = 2
     vertical   = 1
+    sign       = 1
 
     while True:
         for i in range(vertical):
             if v == search:
                 found(v, x, y)
-            y   += 1
+            y   += 1 * sign
             v   += 1
         vertical += 1
-
+        sign *= -1
         for i in range(horizontal):
             if v == search:
                 found(v, x, y)
-            x   -= 1
-            v   += 1
-        horizontal += 1
-
-        for i in range(vertical):
-            if v == search:
-                found(v, x, y)
-            y   -= 1
-            v   += 1
-        vertical += 1
-
-        for i in range(horizontal):
-            if v == search:
-                found(v, x, y)
-            x   += 1
+            x   += 1 * sign
             v   += 1
         horizontal += 1
 
